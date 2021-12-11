8831fb6f4a316337ac81932a6cb693314f2735c4
Noiman <noiman762@gmail.com>
2021-12-11 17:54:25 +0300

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+  },
+
+  setup() {
+  },
+});
+</script>
